b16d21a5c4fb594e295acc58c0824d50373c8432
Aiden Graham <aiden0405@gmail.com>
2025-10-27 15:11:00 -0500

    submission 1

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import employees from "./db/employees.js";
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Hello employees!");
+});
+
+app.get("/employees", (request, response) => {
+  response.send(employees);
+});
+
+app.get("/employees/random", (request, response) => {
+  const max = employees.length;
+  const idRandom = Math.floor(Math.random() * max);
+
+  const employee = employees[idRandom];
+
+  response.send(employee);
+});
+
+app.get("/employees/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const employee = employees.find((e) => e.id === id);
+  if (employee) {
+    response.status(200).send(employee);
+  } else {
+    response.status(404).send("There is no employee with that ID.");
+  }
+});
+
+export default app;
